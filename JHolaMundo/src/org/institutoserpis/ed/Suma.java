89dcf61a97ea827e386c2821b1adf435469a50d3
pedrom <1dam.pgarcia@institutoserpis.org>
2018-10-01 09:34:29 +0200

    Añadido JHolaMundo con eclipse

---
+++ JHolaMundo/src/org/institutoserpis/ed/Suma.java
@@ -0,0 +1,14 @@
+package org.institutoserpis.ed;
+
+import java.util.Scanner;
+
+public class Suma {
+
+	public static void main(String[] args) {
+		Scanner tcl = new Scanner(System.in);
+		System.out.println("Dame 2 números para sumar: ");
+		num1 = 
+		num2 =
+	}
+
+}
