e4ae36f6a81e6b407e1c2c4c97bdbf47a1d04cd2
pedrom <1dam.pgarcia@institutoserpis.org>
2018-11-19 09:41:26 +0100

    CHolaMundo y CSuma

---
+++ CSuma/CSuma/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CSuma
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            decimal primerNumero = readDecimal("Primer número: ");
+            decimal segundoNumero = readDecimal("Segundo número: ");
+
+            Console.Write("Suma = " + (primerNumero + segundoNumero));
+
+            /*Console.Write("Primer número: ");
+            string stringPrimerNumero = Console.ReadLine();
+            int primerNumero = int.Parse(stringPrimerNumero);
+
+            Console.Write("Segundo número: ");
+            string stringSegundoNumero = Console.ReadLine();
+            decimal segundoNumero = decimal.Parse(stringSegundoNumero);
+
+            Console.Write("Suma = " + (primerNumero + segundoNumero));*/
+        }
+
+        public static decimal readDecimal(string label){
+            Console.Write(label);
+            string stringDecimal = Console.ReadLine();
+            return decimal.Parse(stringDecimal);
+        }
+
+    }
+}
