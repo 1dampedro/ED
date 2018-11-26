2e9a42cf0e776c7b93fbe10878e31a97d445dbb6
pedrom <1dam.pgarcia@institutoserpis.org>
2018-11-26 09:53:01 +0100

    CVector creado

---
+++ CVector/CVector/VectorTest.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using System;
+namespace CVector
+{
+    [TestFixture()]
+    public class VectorTest
+    {
+        [Test()]
+        public void BinarySearch()
+        {
+            Assert.AreEqual(-1, Vector.BinarySearch(new int[]{1,3,5,10,12}, 10));//Valor esperado , valor obtenido
+        }
+    }
+}
