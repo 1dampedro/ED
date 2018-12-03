a591fdd5331a2c7072394ad1ef987967f3d52f29
pedrom <1dam.pgarcia@institutoserpis.org>
2018-12-03 11:18:57 +0100

    CGtk añadido

---
+++ CGtk/CGtk/MainWindow.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Gtk;
+
+public partial class MainWindow : Gtk.Window
+{
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+        buttonAceptar.Clicked += delegate {
+            String nombre = entryNombre.Text;
+            labelHola.Text = "Hola " + nombre;
+            Console.WriteLine("Hecho click en buttonAceptar con nombre " + nombre);
+        };
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+}
