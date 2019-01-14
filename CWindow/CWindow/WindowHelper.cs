6d6ebeb50695ad194fa5e714ee5c68daddcec9ee
pedrom <1dam.pgarcia@institutoserpis.org>
2019-01-14 09:44:09 +0100

    CWindow

---
+++ CWindow/CWindow/WindowHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Gtk;
+
+namespace CWindow
+{
+    public class WindowHelper
+    {
+        public static bool Confirm(Window window, String msg){
+            MessageDialog messageDialog = new MessageDialog(
+                window, DialogFlags.Modal, MessageType.Question,
+                ButtonsType.YesNo, msg);
+
+            messageDialog.Title = window.Title;
+            ResponseType response = (ResponseType)messageDialog.Run();
+
+            return response == 0;
+        }
+    }
+}
