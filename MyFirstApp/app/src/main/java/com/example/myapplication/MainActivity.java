0871ca8bcddaf6fe2d47154f200e47118afa46e1
pedrom <1dam.pgarcia@institutoserpis.org>
2019-01-28 09:52:39 +0100

    MyFirstApp aplicacion creada con respuesta

---
+++ MyFirstApp/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view) {
+        EditText editText = findViewById(R.id.editText);
+        System.out.println("Ejecutando sendMessage editText.getText().toString()= "
+                + editText.getText().toString());
+
+    }
+
+}
